2c0ed7acae6b6eaa4d288b441f4a760ae136ed27
bolijesus <bolijesus98@gmail.com>
2023-03-11 14:38:30 -0500

    Fin seecion typeScript (Legacy)

--- typescript/app.js
+++ typescript/app.js
@@ -1,13 +1,46 @@
 "use strict";
-(function () {
-    const avenger = {
-        nombre: 'Steve',
-        clave: 'Capitan america',
-        poder: 'DROGO'
+(() => {
+    // Uso de Let y Const
+    let nombre = 'Ricardo Tapia';
+    let edad = 23;
+    let PERSONAJE = {
+        nombre: nombre,
+        edad: edad
     };
-    const extraer = ({ nombre, poder }) => {
-        console.log(nombre);
-        console.log(poder);
+    let batman = {
+        nombre: 'Bruno Díaz',
+        artesMarciales: ['Karate', 'Aikido', 'Wing Chun', 'Jiu-Jitsu']
     };
-    extraer(avenger);
+    // Convertir esta funcion a una funcion de flecha
+    const resultadoDoble = (a, b) => (a + b) * 2;
+    // Función con parametros obligatorios, opcionales y por defecto
+    // donde NOMBRE = obligatorio
+    //       PODER  = opcional
+    //       ARMA   = por defecto = 'arco'
+    function getAvenger(nombre, poder, arma = 'arco') {
+        let mensaje;
+        if (poder) {
+            mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+        }
+        else {
+            mensaje = nombre + ' tiene un ' + poder;
+        }
+    }
+    ;
+    // Cree una clase que permita manejar la siguiente estructura
+    // La clase se debe de llamar rectangulo,
+    // debe de tener dos propiedades:
+    //   * base
+    //   * altura
+    // También un método que calcule el área  =  base * altura,
+    // ese método debe de retornar un numero.
+    class Rectangulo {
+        constructor(base, altura) {
+            this.base = base;
+            this.altura = altura;
+        }
+        area() {
+            return this.base * this.altura;
+        }
+    }
 })();
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,19 +1,56 @@
-(function(){
-     const avenger = {
-        nombre: 'Steve',
-        clave:  'Capitan america',
-        poder:  'DROGO'
-     };
+(()=>{
 
-     const extraer = ({nombre, poder}:any) => {
-        console.log(nombre);
-        console.log(poder);
-        
-     }
+  // Uso de Let y Const
+  let nombre:string = 'Ricardo Tapia';
+  let edad:number = 23;
 
-     extraer(avenger);
+  let PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+  };
+
+
+  // Cree una interfaz que sirva para validar el siguiente objeto
+  interface Heroe{
+    nombre:string;
+    artesMarciales:string[];
+  }
+  let batman:Heroe = {
+    nombre: 'Bruno Díaz',
+    artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
+  }
+
+  // Convertir esta funcion a una funcion de flecha
+  const resultadoDoble = ( a:number, b:number ):number => (a + b) * 2;
+
+  // Función con parametros obligatorios, opcionales y por defecto
+  // donde NOMBRE = obligatorio
+  //       PODER  = opcional
+  //       ARMA   = por defecto = 'arco'
+  function getAvenger( nombre:string, poder?:string, arma:string = 'arco' ){
+    let mensaje;
+    if( poder ){
+      mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+    }else{
+      mensaje = nombre + ' tiene un ' + poder
+    }
+  };
+
+  // Cree una clase que permita manejar la siguiente estructura
+  // La clase se debe de llamar rectangulo,
+  // debe de tener dos propiedades:
+  //   * base
+  //   * altura
+  // También un método que calcule el área  =  base * altura,
+  // ese método debe de retornar un numero.
+
+  class Rectangulo{
+    constructor (public base:number, public altura:number){}
+
+    area():number{
+        return this.base * this.altura;
+    }
+  }
+
 
 })();
-
-
-
