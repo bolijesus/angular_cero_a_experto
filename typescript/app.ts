8177d0015f957e0468fd48e8136154c32e71ee23
bolijesus <bolijesus98@gmail.com>
2023-03-11 19:12:00 -0500

    Creacion de interfaces

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,56 +1,27 @@
-(()=>{
+interface SuperHeroe {
+  nombre: string;
+  edad: number;
+  direccion: Direccion;
+  mostrarDireccion(): string;
+}
+interface Direccion {
+  calle: string;
+  pais: string;
+  ciudad: string;
+}
 
-  // Uso de Let y Const
-  let nombre:string = 'Ricardo Tapia';
-  let edad:number = 23;
-
-  let PERSONAJE = {
-    nombre: nombre,
-    edad: edad
-  };
-
-
-  // Cree una interfaz que sirva para validar el siguiente objeto
-  interface Heroe{
-    nombre:string;
-    artesMarciales:string[];
+const superHeroe: SuperHeroe = {
+  nombre: 'Spiderman',
+  edad: 30,
+  direccion: {
+    calle: 'Main St',
+    pais: 'USA',
+    ciudad: 'NY'
+  },
+  mostrarDireccion() {
+    return `${this.nombre}, ${this.direccion.ciudad}, ${this.direccion.pais}`
   }
-  let batman:Heroe = {
-    nombre: 'Bruno Díaz',
-    artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
-  }
+}
 
-  // Convertir esta funcion a una funcion de flecha
-  const resultadoDoble = ( a:number, b:number ):number => (a + b) * 2;
-
-  // Función con parametros obligatorios, opcionales y por defecto
-  // donde NOMBRE = obligatorio
-  //       PODER  = opcional
-  //       ARMA   = por defecto = 'arco'
-  function getAvenger( nombre:string, poder?:string, arma:string = 'arco' ){
-    let mensaje;
-    if( poder ){
-      mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
-    }else{
-      mensaje = nombre + ' tiene un ' + poder
-    }
-  };
-
-  // Cree una clase que permita manejar la siguiente estructura
-  // La clase se debe de llamar rectangulo,
-  // debe de tener dos propiedades:
-  //   * base
-  //   * altura
-  // También un método que calcule el área  =  base * altura,
-  // ese método debe de retornar un numero.
-
-  class Rectangulo{
-    constructor (public base:number, public altura:number){}
-
-    area():number{
-        return this.base * this.altura;
-    }
-  }
-
-
-})();
+const direccion = superHeroe.mostrarDireccion()
+console.log(direccion);
