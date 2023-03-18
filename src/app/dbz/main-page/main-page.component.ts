64d6b84696ccd6cefa7d941b891ed259f547f353
bolijesus <bolijesus98@gmail.com>
2023-03-18 08:54:30 -0500

    Exports, One y two way binding, @Input('hijo')

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dsz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+  
+  personajes:Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo.nombre = '';
+    this.nuevo.poder = 0;
+
+    this.nuevo = {
+      nombre:'',
+      poder:0
+    }
+  }
+
+}
