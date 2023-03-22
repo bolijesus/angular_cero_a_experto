2bd5fc99859494f26908e2059cc80eafdea84078
bolijesus <bolijesus98@gmail.com>
2023-03-22 09:25:39 -0500

    seccion 7- Finalizacion del proyecto GifsApss

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,24 +1,50 @@
+import { HttpParams, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.terface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _historial:string[] = [];
+  private apiKey: string = 'sc4ID4JL5jKxQmbcDLhiTBF7PSBykJdZ';
+  private servicioUrl: string = 'http://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+  public resultados: Gif[] = []
 
-  get historial(){
+  get historial() {
     return [...this._historial]
   }
 
-  buscarGifs(query:string=''){
-    
+  constructor(private http: HttpClient) {
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+
+  }
+
+  buscarGifs(query: string = '') {
+
     query = query.trim().toLowerCase();
 
     if (!this._historial.includes(query)) {
-      this._historial.unshift(query);      
+      this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
-    
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query)
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params})
+      .subscribe((response) => {
+        console.log(response.data);
+        this.resultados = response.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      })
+
+
   }
 }
