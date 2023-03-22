4a0cd4096fdc7fdc9af046d2307ec63b137fa977
bolijesus <bolijesus98@gmail.com>
2023-03-21 21:13:45 -0500

    proyecto angular limpio, Borrar los estilos de styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public title = 'bases';
-  public numero: number = 10;
-  public base: number = 5;
-
-  acumular(valor: number): void {
-    this.numero += valor;
-  }
 }
