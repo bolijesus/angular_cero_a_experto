78d63553cc1dc5a5edbecebbd1281935b590a95f
bolijesus <bolijesus98@gmail.com>
2023-03-23 11:42:32 -0500

    14. manejo de errores desde el componente.ts

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,9 +1,27 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html'
 })
 export class PorPaisComponent {
+
+  termino: string = '';
+  hayError:boolean = false;
+
+  constructor(private paisService:PaisService){}
+
+  buscar() {
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino).subscribe(
+      (response:any) => {
+        console.log(response);        
+      },(error:HttpErrorResponse)=>{
+        this.hayError = true;        
+      }
+    );
+  }
 
 }
