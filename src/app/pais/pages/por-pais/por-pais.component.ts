502bc7d4b5a2cd8d0ecd32398c8b1a2419bf0fbb
bolijesus <bolijesus98@gmail.com>
2023-03-23 21:55:18 -0500

    @input,@output reutilizacion de tabla e input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -15,8 +15,9 @@ export class PorPaisComponent {
 
   constructor(private paisService:PaisService){}
 
-  buscar() {
+  buscar(termino:string) {
     this.hayError = false;
+    this.termino = termino;
     this.paisService.buscarPais(this.termino).subscribe(
       (paises:Country[]) => {
         this.paises = paises;    
