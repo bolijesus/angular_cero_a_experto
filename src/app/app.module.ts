a65a39a96c647db7d2cf92587dfe2d81466df522
bolijesus <bolijesus98@gmail.com>
2023-04-09 16:25:51 -0500

    M10, V12. Se agregaron las rutas y la redireccion de las mismas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    AppRouterModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
