d439542489427e38637c5741f7fe4255ff226308
bolijesus <bolijesus98@gmail.com>
2023-05-20 10:21:31 -0500

    M16, V5 Componentes y lazyLoad de formularios

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicosComponent,
+    DinamicosComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
-    TemplateRoutingModule
+    TemplateRoutingModule,
+    RouterModule
   ]
 })
 export class TemplateModule { }
